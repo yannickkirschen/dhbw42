ed9ce695cd13cc8ae8599ae737ba7c11bad48d0f
Yannick Kirschen <yannickkirschen@protonmail.com>
2022-05-04 10:52:42 +0200

    feat: enable CORS for all
    Signed-off-by: Yannick Kirschen <yannickkirschen@protonmail.com>

--- server/src/main/java/sh/yannick/dhbw/dhbw42/Main.java
+++ server/src/main/java/sh/yannick/dhbw/dhbw42/Main.java
@@ -4,6 +4,7 @@ import org.springdoc.core.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.*;
 
 @SpringBootApplication
 public class Main {
@@ -19,5 +20,15 @@ public class Main {
     @Bean
     public SpringDocConfigProperties springDocConfigProperties() {
         return new SpringDocConfigProperties();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+            }
+        };
     }
 }
